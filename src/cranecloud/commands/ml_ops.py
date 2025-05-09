114e27aaf3f2e476345370eb37550becbcf32dce
khalifan-kfan <khalifanmuwonge@gmail.com>
2025-05-09 08:46:59 +0300

    add token use to core function

--- src/cranecloud/commands/ml_ops.py
+++ src/cranecloud/commands/ml_ops.py
@@ -24,15 +24,15 @@ def mlops():
     pass
 
 
+# include token
 @mlops.command('create-experiment', help='Create an MLOps experiment.')
-@click.option('--user-id', required=True, help='User ID')
-@click.option('--app-alias', required=True, help='App alias')
-def create_experiment(user_id, app_alias):
+@click.option('--token', required=True, help='User app token.')
+def create_experiment(token):
     """Create a new experiment ."""
     try:
         # Use the core client to do the actual work
         tracking_uri, experiment_id = mlops_client.create_experiment(
-            user_id, app_alias, verbose=True)
+            token=token, verbose=True)
 
         click.echo(f"Experiment created with ID: {experiment_id}")
         return tracking_uri, experiment_id
