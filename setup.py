ce0e0ad9a603b207f99ed19f5ee2cb402a0cdd8e
Mubangizi Allan <mubangizia22@gmail.com>
2025-06-18 15:22:08 +0300

    deployment workflow

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='cranecloud',
     version='0.1.0',
-    package_dir={'': 'src'}, 
+    package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
     entry_points={
